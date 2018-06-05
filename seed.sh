46b6c17843e5bd91f52a60d1ff3e061477a2c6ba
Martin Levesque <levesque.martin@gmail.com>
2018-06-04 20:19:36 -0400

    load .env

--- seed.sh
+++ seed.sh
@@ -1,2 +1,1 @@
-CURRENT_HOST=localhost:4000 DB_USER=root DB_PASSWORD=test1234 HTTP_PORT=4000 HTTPS_PORT=4001  mix run priv/repo/seeds.exs
-#source .env && mix phx.server
+source .env ; mix run priv/repo/seeds.exs
