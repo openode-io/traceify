1e56a7e5870910d7eab84b742dd2352041fea689
Martin Levesque <levesque.martin@gmail.com>
2018-07-12 17:20:37 -0400

    new start

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-docker run -d -p 127.0.0.1:5000:5000 -v $(pwd):/opt/app --env-file=.env traceify bash build-and-run.sh # mix phx.server
+docker run -d --restart unless-stopped -p 127.0.0.1:5000:5000 \
+  -v $(pwd):/opt/app --env-file=.env traceify bash build-and-run.sh 
