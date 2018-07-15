2870fbadbaca73d568e37992921e82b1be3e5806
Martin Levesque <levesque.martin@gmail.com>
2018-07-14 20:47:51 -0400

    fix

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 docker run -d --restart=always -p 127.0.0.1:5000:5000 \
-  -v $(pwd):/opt/app --env-file=.env traceify bash build-and-run.sh
+  -v $(pwd):/opt/app --net="host" --env-file=.env traceify bash build-and-run.sh
